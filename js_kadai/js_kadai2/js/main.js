dd5af4ea1f15eba6a9e4eee11102845efe0e85bc
your git username <your git email>
2020-11-10 22:34:02 +0900

    kadai2

--- js_kadai/js_kadai2/js/main.js
+++ js_kadai/js_kadai2/js/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const $run = document.getElementById('run');
-const $input_fizz = document.getElementById('input-fizz');
-const $input_buzz = document.getElementById('input-buzz');
-const $answer = document.getElementById('answer');
+const run = document.getElementById('run');
+const input_fizz = document.getElementById('input-fizz');
+const input_buzz = document.getElementById('input-buzz');
+const answer = document.getElementById('answer');
 
 const resetResult = () => {
-  while ($answer.firstChild) {
-    $answer.removeChild($answer.firstChild);
+  while (answer.firstChild) {
+    answer.removeChild(answer.firstChild);
   }
 }
 
 const judgeInteger = () => {
-  if($input_fizz.value.match(/^([1-9]\d*|0)$/) && $input_buzz.value.match(/^([1-9]\d*|0)$/)) {
+  if(input_fizz.value.match(/^([1-9]\d*|0)$/) && input_buzz.value.match(/^([1-9]\d*|0)$/)) {
     return cauculateNumber();
   } else {
     return outputResult('整数値を入力してください');
@@ -22,11 +22,11 @@ const judgeInteger = () => {
 const cauculateNumber = () => {
   let index = 1;
   while (index < 100) {
-    if (index % $input_fizz.value === 0 && index % $input_buzz.value === 0) {
+    if (index % input_fizz.value === 0 && index % input_buzz.value === 0) {
       outputResult(`FizzBuzz: ${index}`);
-    } else if (index % $input_fizz.value === 0) {
+    } else if (index % input_fizz.value === 0) {
       outputResult(`Fizz: ${index}`);
-    } else if (index % $input_buzz.value === 0) {
+    } else if (index % input_buzz.value === 0) {
       outputResult(`Buzz: ${index}`);
     }
     index++;
@@ -34,13 +34,13 @@ const cauculateNumber = () => {
 }
 
 const outputResult = (name) => {
-  const $ans_text = document.createTextNode(name);
-  const $add_p = document.createElement("p");
-  $add_p.appendChild($ans_text);
-  $answer.appendChild($add_p);
+  const ans_text = document.createTextNode(name);
+  const add_p = document.createElement("p");
+  add_p.appendChild(ans_text);
+  answer.appendChild(add_p);
 }
 
-$run.onclick = function() {
+run.onclick = function() {
   resetResult();
   judgeInteger();
 };
